3ce8cdd790339c098588f19a78a19a463ef4cc5e
Baykoku-cs <Baykoku@yandex.ru>
2025-07-24 18:55:21 +0300

    Улучшил основной скрипт
    Добавил переход на мини игру.
    Решил проблему с наложением канвасов (не уверен, что полностью).

---
+++ Assets/Scripts/CustomCommands/StartMemoCommand.cs
@@ -0,0 +1,33 @@
+﻿using Naninovel;
+using Naninovel.Commands;
+using Naninovel.UI;
+using UnityEngine;
+
+[CommandAlias("startMemo")]
+public class StartMemoCommand : Command
+{
+    public override UniTask ExecuteAsync(AsyncToken asyncToken = default)
+    {
+
+       // 1. Disable Naninovel input.
+        var inputManager = Engine.GetService<IInputManager>();
+        inputManager.ProcessInput = false;
+
+        // 2. Stop script player.
+        var scriptPlayer = Engine.GetService<IScriptPlayer>();
+        scriptPlayer.Stop();
+
+        Engine.GetService<UIManager>().GetUI<ContinueInputUI>().Hide();
+
+        // 3. Hide text printer.
+        var hidePrinter = new HidePrinter();
+        hidePrinter.ExecuteAsync(asyncToken).Forget();
+
+
+        // 4. Swap Cameras.
+        MemoGameCamera.Instance.ToggleCamera(true);
+        Engine.GetService<ICameraManager>().Camera.enabled = false;
+
+        return UniTask.CompletedTask;
+    }
+}
---
+++ Assets/Scripts/MemoGameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MemoGameCamera : MonoBehaviour
+{
+    public static MemoGameCamera Instance { get; private set; }
+
+    [SerializeField]
+    private Camera _camera;
+
+    private void Awake()
+    {
+        if (Instance is not null)
+        {
+            Debug.LogError("MemoGameCamera instance already exists.");
+        }
+
+        Instance = this;
+    }
+    
+    public void ToggleCamera(bool isEnabled)
+    {
+        _camera.enabled = isEnabled;
+    }
+}
